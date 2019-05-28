509fca6ee2ec214fe84ff35e4c823bd61cb692fa
Yilia <admin@d0w0b.net>
2019-05-28 13:57:31 +0800

    First Commit

---
+++ TeacherWatcher/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TeacherHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TrayNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrayNotify
+            // 
+            this.TrayNotify.ContextMenuStrip = this.contextMenuStrip1;
+            this.TrayNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayNotify.Icon")));
+            this.TrayNotify.Text = "TeacherHelper";
+            this.TrayNotify.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 40);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 36);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 58);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "TeacherHelper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayNotify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TeacherWatcher/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TeacherHelper
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("ProcCmdLine32.dll", CharSet = CharSet.Unicode, EntryPoint = "GetProcCmdLine")]
+        public extern static bool GetProcCmdLine32(uint nProcId, StringBuilder sb, uint dwSizeBuf);
+
+        [DllImport("ProcCmdLine64.dll", CharSet = CharSet.Unicode, EntryPoint = "GetProcCmdLine")]
+        public extern static bool GetProcCmdLine64(uint nProcId, StringBuilder sb, uint dwSizeBuf);
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(WatchProcess));
+            thread.Start();
+        }
+        private void WatchProcess()
+        {
+            string path = @"C:\Files\";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            while (true)
+            {
+                Process[] ps = Process.GetProcesses();
+                foreach (Process p in ps)
+                {
+                    var cmdline = GetCommandLineOfProcess(p);
+                    foreach (string attr in cmdline.Split('"'))
+                    {
+                        var thisattr = attr.ToLower();
+                        if (thisattr.Contains(".pdf") || thisattr.Contains(".doc") || thisattr.Contains(".docx") || thisattr.Contains(".ppt") || thisattr.Contains(".pptx"))
+                        {
+                            Console.WriteLine(attr);
+                            if (!dic.ContainsKey(attr))
+                            {
+                                System.IO.File.Copy(attr, path + Path.GetFileName(attr), true);
+                                dic.Add(attr, "1");
+                            }
+
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        public static string GetCommandLineOfProcess(Process proc)
+        {
+            // max size of a command line is USHORT/sizeof(WCHAR), so we are going
+            // just allocate max USHORT for sanity's sake.
+            var sb = new StringBuilder(0xFFFF);
+            switch (IntPtr.Size)
+            {
+                case 4: GetProcCmdLine32((uint)proc.Id, sb, (uint)sb.Capacity); break;
+                case 8: GetProcCmdLine64((uint)proc.Id, sb, (uint)sb.Capacity); break;
+            }
+            return sb.ToString();
+        }
+    }
+}
